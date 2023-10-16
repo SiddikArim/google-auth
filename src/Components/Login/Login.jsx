3f13933c934da4f77911b094851cde36e65de69d
SiddikArim <arim99999@gmail.com>
2023-10-16 10:39:28 +0600

    sending top to note the work stucture added github and signout

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,29 +1,42 @@
 import React, { useState } from 'react';
 import app from '../../firebase.init';
-import {GoogleAuthProvider, getAuth, signInWithPopup, signOut} from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
 
 const Login = () => {
-    const [ user, setUser] = useState(null)
+    const [user, setUser] = useState(null)
+
     const auth = getAuth(app)
-    const provider = new GoogleAuthProvider()
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
 
     const handleGoogleSignIn = () => {
-       signInWithPopup(auth , provider)
-       .then(result => {
-        const loggedInUser = result.user;
-        console.log('sign in success', loggedInUser);
-        setUser(loggedInUser);
-       })
-       .catch(error =>{
-        console.log(error)
-       })
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const loggedInUser = result.user;
+                console.log('sign in success', loggedInUser);
+                setUser(loggedInUser);
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
-    const handleSignOut = () =>{
+    const handleSignOut = () => {
         signOut(auth)
-        .then( result => {
-            console.log("signed out", result)
-            setUser(null)
+            .then(result => {
+                console.log("signed out", result)
+                setUser(null)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+    const handleGithubSignIn = () =>{
+        signInWithPopup(auth, githubProvider)
+        .then ( result => {
+            const loggedInUser = result.user;
+            console.log(loggedInUser);
+            setUser(loggedInUser);
         })
         .catch(error => {
             console.log(error)
@@ -32,9 +45,15 @@ const Login = () => {
     return (
         <div>
             {/* user ? logout : sign in */}
-           {user ? <button onClick={handleSignOut}>Sign Out</button> :
-            <button onClick={handleGoogleSignIn}>Google login</button>}
-           {user && <div>
+            {user ?
+                <button onClick={handleSignOut}>Sign Out</button> :
+                <div>
+                    <button onClick={handleGoogleSignIn}>Google login</button>
+                    {/* muloto parameter pass korte hole evave function lekhe pore function er por () diye er vitor parameter diye dey */}
+                    <button onClick={()=>{handleGithubSignIn()}}>Github login</button>
+                </div>
+            }
+            {user && <div>
                 <h3>User: {user.displayName}</h3>
                 <p>email: {user.email}</p>
                 <img src={user.photoURL} alt="" />
